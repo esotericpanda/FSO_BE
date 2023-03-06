46db5f8c7e1472286fe9bbad2eb286695289f489
jurada <tokontoszkolne@gmail.com>
2023-03-06 10:50:30 +0100

    Implement adding a new person with error handling

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const app = express()
 
+app.use(express.json())
+
 let persons =[
     { 
       "id": 1,
@@ -57,6 +59,32 @@ app.delete('/api/persons/:id', (request, response)=>{
     response.status(204).end()
 })
 
+const generateId=()=>{
+    return Math.floor(Math.random()*Number.MAX_SAFE_INTEGER)
+}
+
+app.post('/api/persons', (request, response)=>{
+
+    const body = request.body;
+
+    if(!(body.name && body.number)){
+        response.status(400).json({error:'insufficient data: name and number required'})
+        return
+    }
+
+    if(persons.some(p=>p.name===body.name)){
+        response.status(400).json({error: 'name must be unique'})
+    }
+
+    const newPerson = {
+        name: body.name,
+        number: body.number,
+        id: generateId()
+    }
+
+    persons=persons.concat(newPerson)
+    response.json(newPerson)
+})
 
 const PORT=3001
 
