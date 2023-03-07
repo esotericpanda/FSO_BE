d8af2f3d009509fc53f6e849c391f66f1ccbc52b
jurada <tokontoszkolne@gmail.com>
2023-03-07 16:32:31 +0100

    Add front end

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 
 
@@ -10,8 +11,10 @@ morgan.token('body', function (req, res) { return req.method === "POST" ? JSON.s
 const app = express()
 const logger = morgan(':method :url :status :res[content-length] - :response-time ms :body')
 
+app.use(cors())
 app.use(express.json())
 app.use(logger)
+app.use(express.static('build'))
 
 let persons =[
     { 
@@ -96,6 +99,8 @@ app.post('/api/persons', (request, response)=>{
     response.json(newPerson)
 })
 
-const PORT=3001
+const PORT= process.env.PORT || 3001
 
-app.listen(PORT)
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`)
+})
